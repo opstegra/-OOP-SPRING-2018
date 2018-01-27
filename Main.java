7c6a46200167fb1f4c74ae617d1f12251475413a
OPS_TEGRA\Akash <amemon986@gmail.com>
2018-01-27 18:42:02 +0500

    SOlution

---
+++ Main.java
@@ -0,0 +1,10 @@
+package com.company;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println();
+    }
+}
